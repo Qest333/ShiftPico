1f56ce48296d93b8093fbe7c800978b41185e406
P3rch <81705347+P3rch@users.noreply.github.com>
2021-04-03 22:55:25 +0200

    Update code.py
    see if it works

--- code.py
+++ code.py
@@ -1,17 +1,21 @@
-import time
 import board
 import digitalio
 import usb_hid
+
 from adafruit_hid.gamepad import Gamepad
 
-gp = Gamepad(usb_hid.devices)
-
-btn_a = digitalio.DigitalInOut(board.GP19)
-btn_a.direction = digitalio.Direction.INPUT
-btn_a.pull = digitalio.Pull.UP
+shifter = Gamepad(usb_hid.devices)
+button_pins = (board.GP18, board.GP19, board.GP20, board.GP21, board.GP22, board.GP26, board.GP27)
+shifter_buttons = (1, 2, 3, 4, 5, 6, 7)
+buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
+for button in buttons:
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
 
 while True:
-    buttonavalue = btn_a.value
-    if buttonavalue == 0:
-        gp.click_buttons(1)
-        time.sleep(0.2)
+    for i, button in enumerate(buttons):
+        shifter_button_num = shifter_buttons[i]
+        if button.value:
+            shifter.release_buttons(shifter_button_num)
+        else:
+            shifter.press_buttons(shifter_button_num)
