21777819b3135e143146a6b6268f4682f6b934f8
Qest333 <Qest333@lopit321@gmail.com>
2021-04-03 23:59:01 +0200

    moja wersja

--- code.py
+++ code.py
@@ -1,21 +1,25 @@
 import board
 import digitalio
 import usb_hid
-
 from adafruit_hid.gamepad import Gamepad
 
+class gear:
+    def __init__(self, pin):
+        self.pin = pin
+        self.pin.direction = digitalio.Direction.INPUT
+        self.pin.pull = digitalio.Pull.UP
+
 shifter = Gamepad(usb_hid.devices)
-button_pins = (board.GP18, board.GP19, board.GP20, board.GP21, board.GP22, board.GP26, board.GP27)
-shifter_buttons = (1, 2, 3, 4, 5, 6, 7)
-buttons = [digitalio.DigitalInOut(pin) for pin in button_pins]
-for button in buttons:
-    button.direction = digitalio.Direction.INPUT
-    button.pull = digitalio.Pull.UP
+gears = list()
+prevButt = 1
+shifter_pins = [board.GP18, board.GP19, board.GP20, board.GP21, board.GP10, board.GP11, board.GP12]
+
+for pins in shifter_pins:
+    gears.append(gear(digitalio.DigitalInOut(pins)))
 
 while True:
-    for i, button in enumerate(buttons):
-        shifter_button_num = shifter_buttons[i]
-        if button.value:
-            shifter.release_buttons(shifter_button_num)
-        else:
-            shifter.press_buttons(shifter_button_num)
+    buttonvalue = (int)(not gears[0].pin.value) + (int)(not gears[1].pin.value)*2 + (int)(not gears[2].pin.value)*3 + (int)(not gears[3].pin.value)*4 + (int)(not gears[4].pin.value)*5 + (int)(not gears[5].pin.value)*6 + (int)(not gears[6].pin.value)*7
+    if buttonvalue != 0:
+        shifter.press_buttons(buttonvalue)
+        prevButt = buttonvalue
+    else: shifter.release_buttons(prevButt)
