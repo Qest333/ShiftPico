afc689aa9600f38146fe68371d452ed2ee18771f
Qest333 <Qest333@lopit321@gmail.com>
2021-04-04 01:41:44 +0200

    test3

--- code.py
+++ code.py
@@ -18,11 +18,10 @@ pressedButton = 0
 prevButton = 0
 while True:
     buttonvalue = (int)(not gears[0].pin.value)*2 + (int)(not gears[1].pin.value)*3 + (int)(not gears[2].pin.value)*5 + (int)(not gears[3].pin.value)*7 + (int)(not gears[4].pin.value)*11 + (int)(not gears[5].pin.value)*13 + (int)(not gears[6].pin.value)*17
-    print(buttonvalue)
     if pressedButton > prevButton:
         shifter.press_buttons((prevButton-pressedButton)*-1)
+        prevButton = pressedButton
     elif pressedButton < prevButton:
         shifter.release_buttons(prevButton-pressedButton)
+        prevButton = pressedButton
     pressedButton = (buttonvalue != 0)*(1*(buttonvalue%2 == 0) + 2*(buttonvalue%3 == 0) + 3*(buttonvalue%5 == 0) + 4*(buttonvalue%7 == 0) + 5*(buttonvalue%11 == 0) + 6*(buttonvalue%13 == 0) + 7*(buttonvalue%17 == 0))
-    print(pressedButton)
-    time.sleep(0.1)
